a50081e2d98a224eb9495b7d017be1fe5262fcd0
cherkaski <alexcher81@gmail.com>
2019-02-02 00:16:29 -0500

    Created chains branch. Added some chain prefabs.

---
+++ Flame Keeper/Assets/ConnectLinks.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ConnectLinks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<CharacterJoint>().connectedBody = this.transform.parent.GetComponent<Rigidbody>();
+    }
+}
