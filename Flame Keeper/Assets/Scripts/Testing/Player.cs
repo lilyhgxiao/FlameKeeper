6b244103e85a13d7f78b5bf440f25b13544cdffb
Lily <lilyhgxiao@gmail.com>
2019-02-08 22:16:26 -0500

    Made timed collision with water script

---
+++ Flame Keeper/Assets/Scripts/Testing/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int forceMagnitude = 10;
+    public int jumpSpeed = 5;
+
+    private bool blocksDestroyed = false;
+    private Vector3 originalPosition;
+    private Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        this.originalPosition = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Fire1") > 0)
+        {
+			Vector3 vel = body.velocity;
+			vel.y = jumpSpeed;
+			body.velocity = vel;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float zMovement = Input.GetAxis("Vertical");
+
+        Vector3 vel = body.velocity;
+        vel.x = xMovement * forceMagnitude;
+        vel.z = zMovement * forceMagnitude;
+        body.velocity = vel;
+    }
+
+}
---
+++ Flame Keeper/Assets/Scripts/Testing/WaterCollision.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCollision : MonoBehaviour
+{
+    
+    public float waitTime = 3f;
+    public GameObject player;
+    private bool playerTouching;
+    private bool playerAlive;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTouching = false;
+        playerAlive = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTouching && playerAlive)
+        {
+            timer += Time.deltaTime;
+            player.GetComponent<Renderer>().material.color = Color.blue;
+            if (timer > waitTime)
+            {
+                player.GetComponent<Renderer>().material.color = Color.red;
+                playerAlive = false;
+                timer = 0f;
+            }
+        }
+        else
+        {
+            timer = 0;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        playerTouching = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag("Player"))
+        {
+            return;
+        }
+        playerTouching = false;
+        playerAlive = true;
+        player.GetComponent<Renderer>().material.color = Color.white;
+    }
+}
